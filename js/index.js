cc6045cb4bba0264c62262e698021ae29039e87d
Braedan Royer <royerbraedan@gmail.com>
2026-01-12 22:28:05 -0800

    finished the basic logic to generate passwords. tweaked the hover effects on the password generator button. added a cursor to the password divs to make it clear that clicking them will copy the password

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/* Global Variables */
+const generatePasswordBtn = document.querySelector('.btn-password')
+const allChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_",
+  "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", '"',
+  ",", ".", "<", ">", "/", "?", "~", "`"];
+
+
+/* Functions */
+function randomCharacter() {
+  return Math.floor(Math.random() * allChars.length)
+}
+
+function generatePassword() {
+  let password = ''
+  for (let i = 0; i < 16; i++) {
+    password += allChars[randomCharacter()]
+  }
+  return password
+}
+
+/* Event Handlers */
+generatePasswordBtn.addEventListener('click', () => {
+  document.querySelector("#password_1").textContent = generatePassword();
+  document.querySelector("#password_2").textContent = generatePassword();
+})
