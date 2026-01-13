14fa64776ca1f2283c241ff6b68fdd4d2aabbc24
Braedan Royer <royerbraedan@gmail.com>
2026-01-12 22:59:55 -0800

    made it so users will not accidentally click the word copied instead of the password

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 /* Global Variables */
 const generatePasswordBtn = document.querySelector('.btn-password')
+const passwordElements = document.querySelectorAll('.password_div')
 const allChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
   "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
@@ -11,20 +12,40 @@ const allChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C
 
 /* Functions */
 function randomCharacter() {
-  return Math.floor(Math.random() * allChars.length)
+  return allChars[Math.floor(Math.random() * allChars.length)]
 }
 
 function generatePassword() {
   let password = ''
   for (let i = 0; i < 16; i++) {
-    password += allChars[randomCharacter()]
+    password += randomCharacter();
   }
   return password
 }
 
 /* Event Handlers */
 generatePasswordBtn.addEventListener('click', () => {
-  document.querySelector("#password_1").textContent = generatePassword();
-  document.querySelector("#password_2").textContent = generatePassword();
+  passwordElements.forEach(el => el.textContent = generatePassword())
   generatePasswordBtn.blur();
+})
+
+passwordElements.forEach(el => {
+  let isCopied = false;
+
+  el.addEventListener('click', async () => {
+    if (isCopied) return;
+    const password = el.textContent;
+    try {
+      await navigator.clipboard.writeText(password)
+      isCopied = true;
+      el.textContent = 'Copied!'
+
+      setTimeout(() => {
+        el.textContent = password
+        isCopied = false
+      }, 1000)
+    } catch(err) {
+      console.error('Failed to copy:', err)
+    }
+  })
 })
